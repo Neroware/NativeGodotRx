b263e40bf74fd0c34b38b63a2d403ec8f7fe9cba
Raoul Zebisch <neroware@freenet.de>
2023-08-06 20:56:22 +0200

    Added remaining observers

---
+++ cpp/reactivex/lib/observer/observeonobserver.cpp
@@ -0,0 +1,20 @@
+#include "observer/observeonobserver.h"
+
+namespace rx::observer {
+
+void ObserveOnObserver::_on_next_core(const Variant& item) {
+    this->ensure_active();
+    ScheduledObserver::_on_next_core(item);
+}
+
+void ObserveOnObserver::_on_error_core(const std::exception& error) {
+    this->ensure_active();
+    ScheduledObserver::_on_error_core(error);
+}
+
+void ObserveOnObserver::_on_completed_core() {
+    this->ensure_active();
+    ScheduledObserver::_on_completed_core();
+}
+
+} // END namespace rx::observer
