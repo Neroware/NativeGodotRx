66ba7ec910db72c6825aeb78165ee35c65c69211
Raoul Zebisch <neroware@freenet.de>
2023-08-15 19:19:08 +0200

    Refactored catch (observer memory leak solved)

--- cpp/reactivex/include/observable/catch.h
+++ cpp/reactivex/include/observable/catch.h
@@ -14,12 +14,10 @@ namespace rx::observable {
 
 template<typename T>
 static std::shared_ptr<Observable> catch_with_iterable_(const T& sources) {
-
     auto _end = sources.end();
     auto _it = std::make_shared<typename T::const_iterator>(sources.begin());
 
     subscription_t subscribe = SUBSCRIBE(nullptr) {
-
         auto _scheduler = scheduler_ ? scheduler_ : CurrentThreadScheduler::singleton();
 
         auto subscription = std::make_shared<SerialDisposable>();
@@ -27,12 +25,15 @@ static std::shared_ptr<Observable> catch_with_iterable_(const T& sources) {
         auto last_exception = std::make_shared<std::exception_ptr>();
         auto is_disposed = std::make_shared<bool>(false);
 
-        auto action = [=](const std::shared_ptr<SchedulerBase>& scheduler__, const Variant& state, const auto& _observer, const auto& _action) -> std::shared_ptr<DisposableBase> {
-
+        auto action = [=](const std::shared_ptr<SchedulerBase>& scheduler__, const Variant& state, const auto& _action) -> std::shared_ptr<DisposableBase> {
             on_error_t on_error = [=](const std::exception_ptr& exn) {
                 *last_exception = exn;
-                cancelable->set_disposable(_scheduler->schedule(ACTION { return _action(scheduler__, state, _observer, _action); }));
+                cancelable->set_disposable(_scheduler->schedule(ACTION { return _action(scheduler__, state, _action); }));
             };
+
+            if (*is_disposed) {
+                return nullptr;
+            }
 
             std::shared_ptr<ObservableBase> current;
             try { 
@@ -54,18 +55,18 @@ static std::shared_ptr<Observable> catch_with_iterable_(const T& sources) {
                 return nullptr;
             }
             auto d = std::make_shared<SingleAssignmentDisposable>();
+            subscription->set_disposable(d);
             d->set_disposable(current->subscribe(
-                [_observer](const Variant& value) { _observer->on_next(value); },
+                [observer](const Variant& value) { observer->on_next(value); },
                 on_error,
-                [_observer]() { _observer->on_completed(); },
+                [observer]() { observer->on_completed(); },
                 scheduler_
             ));
 
             return nullptr;
-
         };
 
-        cancelable->set_disposable(_scheduler->schedule(ACTION { return action(scheduler__, state, observer, action); } ));
+        cancelable->set_disposable(_scheduler->schedule(ACTION { return action(scheduler__, state, action); } ));
 
         dispose_t dispose = [is_disposed]() {
             *is_disposed = true;
--- cpp/reactivex/project/main.gd
+++ cpp/reactivex/project/main.gd
@@ -3,189 +3,138 @@ extends "res://test_base.gd"
 var custom_signal_emitted = null
 
 func _ready():
+	var scheduler = RxScheduler.CurrentThreadSchedulerSingleton()
 	
-#	GDRx.iter([1, 2, 3]).foreach(func(i): print("> ", i))
+	RxObservable.catch([
+		RxObservable.throw("Planned exception!", scheduler),
+		RxObservable.throw("Planned exception 2!", scheduler),
+		RxObservable.just(42, scheduler),
+		RxObservable.just(123, scheduler)
+	]) \
+	.subscribe(func(i): print("i> ", i), func(e): print("ERR: ", e), func(): print(":)")) \
+	.dispose_with(self)
 	
-#	var scheduler = RxScheduler.CurrentThreadSchedulerSingleton()
-#	const N_REPEATS = 10000
-#	var action = func(__ = null, ___ = null):
-#		pass
-#	
-#	var t0 = Time.get_ticks_msec()
-#	for i in range(N_REPEATS):
-#		scheduler.schedule(action)
-#	var t1 = Time.get_ticks_msec()
-#	print("DT: ", (t1 - t0) / 1000.0, "s")
+#	var obs_defer = RxObservable.defer(func(__ : RxScheduler): return RxObservable.just("Deferred!"))
+#	obs_defer.subscribe(func(i): print("i> ", i)).dispose_with(self)
 	
-#	print(">>> Main Thread: ", OS.get_thread_caller_id())
-#	
-#	var nts = RxScheduler.NewThreadScheduler()
-#	var nested_action = func(__ = null, ___ = null):
-#		print("Scheduled on new thread: ", OS.get_thread_caller_id(), ":", OS.get_thread_caller_id())
-#	var action = func(__ = null, ___ = null):
-#		RxScheduler.CurrentThreadSchedulerSingleton().schedule(nested_action)
-#	for i in range(1):
-#		nts.schedule(action).dispose_with(self)
-#	#self.disp_member.dispose()
-#	
-#	var empty_obs : RxObservable = RxObservable.empty()
-#	empty_obs.subscribe(func(i): print(":("), func(e): print("ERR: ", e), func(): print(":)"))
-#	
-#	const N_REPEATS = 100000
-#	var obs = RxObservable.empty()
-#	var t0 = Time.get_ticks_msec()
-#	for i in range(N_REPEATS):
-#		obs.subscribe().dispose_with(self)
-#	var t1 = Time.get_ticks_msec()
-#	print("DT: ", (t1 - t0) / 1000.0, "s ; ", (t1 - t0) / float(N_REPEATS), "ms per sub")
+	get_tree().quit()
 	
-#	RxObservable.just(42).subscribe(func(i): print("i> ", i), func(e): print("ERR: ", e), func(): print(":)")).dispose_with(self)
-	
-	#var obs_list : Array = [
-	#	RxObservable.throw("Planned exception!"),
-	#	RxObservable.just(42),
-	#	RxObservable.just(123)
-	#]
-	#var it = GDRx.iter(obs_list)
-	#while it.has_next():
-	#	print("> ", it.next())
-	
-	var cts = RxScheduler.CurrentThreadSchedulerSingleton()
-	
-#	RxObservable.catch([
-#		RxObservable.throw("Planned exception!"),
-#		RxObservable.throw("Planned exception 2!"),
-#		RxObservable.just(42, cts),
-#		RxObservable.just(123)
-#	]) \
-#	.subscribe(func(i): print("i> ", i), func(e): print("ERR: ", e), func(): print(":)")) \
-#	.dispose_with(self)
-	
-	var obs_defer = RxObservable.defer(func(__ : RxScheduler): return RxObservable.just("Deferred!"))
-	obs_defer.subscribe(func(i): print("i> ", i)).dispose_with(self)
-	
-#	RxObservable.throw("Planned exception!").subscribe(null, func(e): print("ERR: ", e)).dispose_with(self)
-	
-#	RxObservable.throw("Aloha!").subscribe(func(i): print("i>", i), func(e): print("Err: ", e)).dispose_with(self)
-	
-	return
-	
-	var example: Example = $Example
-
-	# Signal.
-	example.emit_custom_signal("Button", 42)
-	assert_equal(custom_signal_emitted, ["Button", 42])
-
-	# To string.
-	assert_equal(example.to_string(),'Example:[ GDExtension::Example <--> Instance ID:%s ]' % example.get_instance_id())
-	# It appears there's a bug with instance ids :-(
-	#assert_equal($Example/ExampleMin.to_string(), 'ExampleMin:[Wrapped:%s]' % $Example/ExampleMin.get_instance_id())
-
-	# Call static methods.
-	assert_equal(Example.test_static(9, 100), 109);
-	# It's void and static, so all we know is that it didn't crash.
-	Example.test_static2()
-
-	# Property list.
-	example.property_from_list = Vector3(100, 200, 300)
-	assert_equal(example.property_from_list, Vector3(100, 200, 300))
-
-	# Call simple methods.
-	example.simple_func()
-	assert_equal(custom_signal_emitted, ['simple_func', 3])
-	example.simple_const_func()
-	assert_equal(custom_signal_emitted, ['simple_const_func', 4])
-
-	# Pass custom reference.
-	assert_equal(example.custom_ref_func(null), -1)
-	var ref1 = ExampleRef.new()
-	ref1.id = 27
-	assert_equal(example.custom_ref_func(ref1), 27)
-	ref1.id += 1;
-	assert_equal(example.custom_const_ref_func(ref1), 28)
-
-	# Pass core reference.
-	assert_equal(example.image_ref_func(null), "invalid")
-	assert_equal(example.image_const_ref_func(null), "invalid")
-	var image = Image.new()
-	assert_equal(example.image_ref_func(image), "valid")
-	assert_equal(example.image_const_ref_func(image), "valid")
-
-	# Return values.
-	assert_equal(example.return_something("some string"), "some string42")
-	assert_equal(example.return_something_const(), get_viewport())
-	var null_ref = example.return_empty_ref()
-	assert_equal(null_ref, null)
-	var ret_ref = example.return_extended_ref()
-	assert_not_equal(ret_ref.get_instance_id(), 0)
-	assert_equal(ret_ref.get_id(), 0)
-	assert_equal(example.get_v4(), Vector4(1.2, 3.4, 5.6, 7.8))
-	assert_equal(example.test_node_argument(example), example)
-
-	# VarArg method calls.
-	var var_ref = ExampleRef.new()
-	assert_not_equal(example.extended_ref_checks(var_ref).get_instance_id(), var_ref.get_instance_id())
-	assert_equal(example.varargs_func("some", "arguments", "to", "test"), 4)
-	assert_equal(example.varargs_func_nv("some", "arguments", "to", "test"), 46)
-	example.varargs_func_void("some", "arguments", "to", "test")
-	assert_equal(custom_signal_emitted, ["varargs_func_void", 5])
-
-	# Method calls with default values.
-	assert_equal(example.def_args(), 300)
-	assert_equal(example.def_args(50), 250)
-	assert_equal(example.def_args(50, 100), 150)
-
-	# Array and Dictionary
-	assert_equal(example.test_array(), [1, 2])
-	assert_equal(example.test_tarray(), [ Vector2(1, 2), Vector2(2, 3) ])
-	assert_equal(example.test_dictionary(), {"hello": "world", "foo": "bar"})
-	var array: Array[int] = [1, 2, 3]
-	assert_equal(example.test_tarray_arg(array), 6)
-
-	# String += operator
-	assert_equal(example.test_string_ops(), "ABCĎE")
-
-	# UtilityFunctions::str()
-	assert_equal(example.test_str_utility(), "Hello, World! The answer is 42")
-
-	# Test converting string to char* and doing comparison.
-	assert_equal(example.test_string_is_fourty_two("blah"), false)
-	assert_equal(example.test_string_is_fourty_two("fourty two"), true)
-
-	# PackedArray iterators
-	assert_equal(example.test_vector_ops(), 105)
-
-	# Properties.
-	assert_equal(example.group_subgroup_custom_position, Vector2(0, 0))
-	example.group_subgroup_custom_position = Vector2(50, 50)
-	assert_equal(example.group_subgroup_custom_position, Vector2(50, 50))
-
-	# Constants.
-	assert_equal(Example.FIRST, 0)
-	assert_equal(Example.ANSWER_TO_EVERYTHING, 42)
-	assert_equal(Example.CONSTANT_WITHOUT_ENUM, 314)
-
-	# BitFields.
-	assert_equal(Example.FLAG_ONE, 1)
-	assert_equal(Example.FLAG_TWO, 2)
-	assert_equal(example.test_bitfield(0), 0)
-	assert_equal(example.test_bitfield(Example.FLAG_ONE | Example.FLAG_TWO), 3)
-
-	# RPCs.
-	assert_equal(example.return_last_rpc_arg(), 0)
-	example.test_rpc(42)
-	assert_equal(example.return_last_rpc_arg(), 42)
-	example.test_send_rpc(100)
-	assert_equal(example.return_last_rpc_arg(), 100)
-
-	# Virtual method.
-	var event = InputEventKey.new()
-	event.key_label = KEY_H
-	event.unicode = 72
-	get_viewport().push_input(event)
-	assert_equal(custom_signal_emitted, ["_input: H", 72])
-
-	exit_with_status()
+#	var example: Example = $Example
+#
+#	# Signal.
+#	example.emit_custom_signal("Button", 42)
+#	assert_equal(custom_signal_emitted, ["Button", 42])
+#
+#	# To string.
+#	assert_equal(example.to_string(),'Example:[ GDExtension::Example <--> Instance ID:%s ]' % example.get_instance_id())
+#	# It appears there's a bug with instance ids :-(
+#	#assert_equal($Example/ExampleMin.to_string(), 'ExampleMin:[Wrapped:%s]' % $Example/ExampleMin.get_instance_id())
+#
+#	# Call static methods.
+#	assert_equal(Example.test_static(9, 100), 109);
+#	# It's void and static, so all we know is that it didn't crash.
+#	Example.test_static2()
+#
+#	# Property list.
+#	example.property_from_list = Vector3(100, 200, 300)
+#	assert_equal(example.property_from_list, Vector3(100, 200, 300))
+#
+#	# Call simple methods.
+#	example.simple_func()
+#	assert_equal(custom_signal_emitted, ['simple_func', 3])
+#	example.simple_const_func()
+#	assert_equal(custom_signal_emitted, ['simple_const_func', 4])
+#
+#	# Pass custom reference.
+#	assert_equal(example.custom_ref_func(null), -1)
+#	var ref1 = ExampleRef.new()
+#	ref1.id = 27
+#	assert_equal(example.custom_ref_func(ref1), 27)
+#	ref1.id += 1;
+#	assert_equal(example.custom_const_ref_func(ref1), 28)
+#
+#	# Pass core reference.
+#	assert_equal(example.image_ref_func(null), "invalid")
+#	assert_equal(example.image_const_ref_func(null), "invalid")
+#	var image = Image.new()
+#	assert_equal(example.image_ref_func(image), "valid")
+#	assert_equal(example.image_const_ref_func(image), "valid")
+#
+#	# Return values.
+#	assert_equal(example.return_something("some string"), "some string42")
+#	assert_equal(example.return_something_const(), get_viewport())
+#	var null_ref = example.return_empty_ref()
+#	assert_equal(null_ref, null)
+#	var ret_ref = example.return_extended_ref()
+#	assert_not_equal(ret_ref.get_instance_id(), 0)
+#	assert_equal(ret_ref.get_id(), 0)
+#	assert_equal(example.get_v4(), Vector4(1.2, 3.4, 5.6, 7.8))
+#	assert_equal(example.test_node_argument(example), example)
+#
+#	# VarArg method calls.
+#	var var_ref = ExampleRef.new()
+#	assert_not_equal(example.extended_ref_checks(var_ref).get_instance_id(), var_ref.get_instance_id())
+#	assert_equal(example.varargs_func("some", "arguments", "to", "test"), 4)
+#	assert_equal(example.varargs_func_nv("some", "arguments", "to", "test"), 46)
+#	example.varargs_func_void("some", "arguments", "to", "test")
+#	assert_equal(custom_signal_emitted, ["varargs_func_void", 5])
+#
+#	# Method calls with default values.
+#	assert_equal(example.def_args(), 300)
+#	assert_equal(example.def_args(50), 250)
+#	assert_equal(example.def_args(50, 100), 150)
+#
+#	# Array and Dictionary
+#	assert_equal(example.test_array(), [1, 2])
+#	assert_equal(example.test_tarray(), [ Vector2(1, 2), Vector2(2, 3) ])
+#	assert_equal(example.test_dictionary(), {"hello": "world", "foo": "bar"})
+#	var array: Array[int] = [1, 2, 3]
+#	assert_equal(example.test_tarray_arg(array), 6)
+#
+#	# String += operator
+#	assert_equal(example.test_string_ops(), "ABCĎE")
+#
+#	# UtilityFunctions::str()
+#	assert_equal(example.test_str_utility(), "Hello, World! The answer is 42")
+#
+#	# Test converting string to char* and doing comparison.
+#	assert_equal(example.test_string_is_fourty_two("blah"), false)
+#	assert_equal(example.test_string_is_fourty_two("fourty two"), true)
+#
+#	# PackedArray iterators
+#	assert_equal(example.test_vector_ops(), 105)
+#
+#	# Properties.
+#	assert_equal(example.group_subgroup_custom_position, Vector2(0, 0))
+#	example.group_subgroup_custom_position = Vector2(50, 50)
+#	assert_equal(example.group_subgroup_custom_position, Vector2(50, 50))
+#
+#	# Constants.
+#	assert_equal(Example.FIRST, 0)
+#	assert_equal(Example.ANSWER_TO_EVERYTHING, 42)
+#	assert_equal(Example.CONSTANT_WITHOUT_ENUM, 314)
+#
+#	# BitFields.
+#	assert_equal(Example.FLAG_ONE, 1)
+#	assert_equal(Example.FLAG_TWO, 2)
+#	assert_equal(example.test_bitfield(0), 0)
+#	assert_equal(example.test_bitfield(Example.FLAG_ONE | Example.FLAG_TWO), 3)
+#
+#	# RPCs.
+#	assert_equal(example.return_last_rpc_arg(), 0)
+#	example.test_rpc(42)
+#	assert_equal(example.return_last_rpc_arg(), 42)
+#	example.test_send_rpc(100)
+#	assert_equal(example.return_last_rpc_arg(), 100)
+#
+#	# Virtual method.
+#	var event = InputEventKey.new()
+#	event.key_label = KEY_H
+#	event.unicode = 72
+#	get_viewport().push_input(event)
+#	assert_equal(custom_signal_emitted, ["_input: H", 72])
+#
+#	exit_with_status()
 
 func _on_Example_custom_signal(signal_name, value):
 	custom_signal_emitted = [signal_name, value]
