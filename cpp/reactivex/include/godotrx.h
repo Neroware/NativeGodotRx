52e407dfba8486fb0226b778e74972df6221c86a
Raoul Zebisch <neroware@freenet.de>
2023-09-09 12:43:21 +0200

    Inititiate refactor

--- cpp/reactivex/include/godotrx.h
+++ cpp/reactivex/include/godotrx.h
@@ -8,7 +8,7 @@
 #include <godot_cpp/variant/string.hpp>
 #include <godot_cpp/core/class_db.hpp>
 
-#include "internal/concurrency.h"
+/* #include "internal/concurrency.h"
 #include "internal/iterator.h"
 #include "internal/thread.h"
 #include "internal/weakkeydictionary.h"
@@ -19,14 +19,14 @@
 #include "scheduler/currentthreadscheduler.h"
 #include "scheduler/immediatescheduler.h"
 #include "scheduler/timeoutscheduler.h"
-#include "scheduler/scenetreetimeoutscheduler.h"
+#include "scheduler/scenetreetimeoutscheduler.h" */
 
 #define GDRX_SINGLETON_NAME "GDRx"
 #define GDRX __GDRxSingleton__::singleton()
 // #define GDRX Ref<__GDRxSingleton__>(Engine::get_singleton()->get_singleton(GDRX_SINGLETON_NAME))
 
 using namespace godot;
-using namespace rx::scheduler;
+// using namespace rx::scheduler;
 
 namespace rx {
 
@@ -35,30 +35,30 @@ class __GDRxSingleton__ : public RefCounted {
 
 public:
     /* Thread manager singleton */
-    const std::shared_ptr<ThreadManager> THREAD_MANAGER = std::make_shared<ThreadManager>();
+    // const std::shared_ptr<ThreadManager> THREAD_MANAGER = std::make_shared<ThreadManager>();
     /* Main thread dummy */
-    const Ref<RxThread> MAIN_THREAD = memnew(RxMainThread);
+    // const Ref<RxThread> MAIN_THREAD = memnew(RxMainThread);
 
     /* Scheduler singletons */
-    weakkey_dictionary<variant_key_t, weakkey_dictionary<variant_key_t, std::shared_ptr<CurrentThreadScheduler>>>CurrentThreadScheduler_global_;
+    /* weakkey_dictionary<variant_key_t, weakkey_dictionary<variant_key_t, std::shared_ptr<CurrentThreadScheduler>>>CurrentThreadScheduler_global_;
     const std::shared_ptr<_CurrentThreadScheduler_Local> CurrentThreadScheduler_local_ = _CurrentThreadScheduler_Local::get();
     const std::shared_ptr<ImmediateScheduler> ImmediateScheduler_ = ImmediateScheduler::get();
     const std::shared_ptr<TimeoutScheduler> TimeoutScheduler_ = TimeoutScheduler::get();
-    std::map<uint8_t, std::shared_ptr<SceneTreeTimeoutScheduler>> SceneTreeTimeoutScheduler_;
+    std::map<uint8_t, std::shared_ptr<SceneTreeTimeoutScheduler>> SceneTreeTimeoutScheduler_; */
 
     /* Not Set */
-    const Ref<NotSet> NOT_SET = memnew(NotSet);
+    // const Ref<NotSet> NOT_SET = memnew(NotSet);
 
 private:
     static __GDRxSingleton__* p_instance;
 
 protected:
 	static inline void _bind_methods() {
-        ClassDB::bind_method(D_METHOD("iter", "it"), &__GDRxSingleton__::iter);
+        /* ClassDB::bind_method(D_METHOD("iter", "it"), &__GDRxSingleton__::iter);
         ClassDB::bind_method(D_METHOD("to_iterable", "it"), &__GDRxSingleton__::to_iterable);
         ClassDB::bind_method(D_METHOD("foreach", "it", "action"), &__GDRxSingleton__::foreach);
         ClassDB::bind_method(D_METHOD("enumerate", "it", "action"), &__GDRxSingleton__::enumerate);
-        ClassDB::bind_method(D_METHOD("from_notifier", "handler"), &__GDRxSingleton__::from_notifier);
+        ClassDB::bind_method(D_METHOD("from_notifier", "handler"), &__GDRxSingleton__::from_notifier); */
     }
 
 public:
@@ -70,27 +70,27 @@ public:
         // Singleton values
         {
             __GDRxSingleton__::p_instance = this;
-            CurrentThreadScheduler::cls = std::make_shared<variant_key_t>("CurrentThreadScheduler");
+            // CurrentThreadScheduler::cls = std::make_shared<variant_key_t>("CurrentThreadScheduler");
         }
 
         // Insert Main Thread
-        {
+        /* {
             std::unique_lock<std::shared_mutex> writeLock(this->THREAD_MANAGER->thread_registry.first);
             auto main_thread_id = std::this_thread::get_id();
             this->THREAD_MANAGER->thread_registry.second[main_thread_id] = MAIN_THREAD;
-        }
+        } */
 
         // Scheduler Singletons
-        {
+        /* {
             for (auto i = 0; i < 8; i++) {
                 this->SceneTreeTimeoutScheduler_[i] = SceneTreeTimeoutScheduler::get(i & 0b100, i & 0b10, i & 0b1);
             }
-        }
+        } */
     }
 
     ~__GDRxSingleton__(){}
 
-    inline Ref<RxIteratorBase> iter(const Variant& iterable) {
+    /* inline Ref<RxIteratorBase> iter(const Variant& iterable) {
         return rx::iterator::iter(iterable);
     }
     inline Ref<RxIterableBase> to_iterable(const Variant& iterable) {
@@ -104,7 +104,7 @@ public:
     }
     inline Ref<RxObserverBase> from_notifier(const Callable& handler) {
         return RxObserverBase::wrap(rx::from_notifier(notification_handler_cb(handler)));
-    }
+    } */
 };
 
 inline __GDRxSingleton__* __GDRxSingleton__::p_instance = nullptr;
--- cpp/reactivex/src/register_types.cpp
+++ cpp/reactivex/src/register_types.cpp
@@ -16,24 +16,6 @@
 #include "tests.h"
 
 #include "godotrx.h"
-// exception
-#include "exception/exceptionwrapper.h"
-// internal
-#include "internal/time.h"
-#include "internal/thread.h"
-#include "internal/utils.h"
-// wrappers
-#include "wrapper/abstract.h"
-#include "wrapper/scheduler.h"
-#include "wrapper/disposable.h"
-#include "wrapper/observer.h"
-#include "wrapper/observable.h"
-#include "wrapper/subject.h"
-#include "wrapper/notificationwrapper.h"
-// disposable
-#include "disposable/autodisposer.h"
-// scheduler
-#include "scheduler/scenetreetimeoutscheduler.h"
 
 using namespace godot;
 
@@ -48,64 +30,6 @@ void initialize_reactivex_module(ModuleInitializationLevel p_level) {
 	ClassDB::register_class<ExampleVirtual>(true);
 	ClassDB::register_abstract_class<ExampleAbstract>();
 
-	// exception
-	ClassDB::register_abstract_class<rx::exception::RxError>();
-	// internal
-	ClassDB::register_abstract_class<rx::AbsoluteTime>();
-	ClassDB::register_abstract_class<rx::RelativeTime>();
-	ClassDB::register_class<rx::ItEnd>();
-	ClassDB::register_class<rx::RxThread>();
-	ClassDB::register_class<rx::RxMainThread>();
-	ClassDB::register_class<rx::NotSet>();
-	// abstract
-	ClassDB::register_abstract_class<rx::wrappers::RxLockBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxDisposableBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxIteratorBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxIterableBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxSchedulerBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxObserverBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxObservableBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxObservable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxSubjectBase>();
-	ClassDB::register_abstract_class<rx::wrappers::RxNotification>();
-	// disposable
-	ClassDB::register_abstract_class<rx::disposable::RxAutoDisposer>();
-	ClassDB::register_abstract_class<rx::wrappers::RxBooleanDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxSingleAssignmentDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxMultiAssignmentDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxSerialDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxRefCountDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxScheduledDisposable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxCompositeDisposable>();
-	// scheduler
-	ClassDB::register_abstract_class<rx::scheduler::RxSceneTreeTimeout>();
-	ClassDB::register_abstract_class<rx::wrappers::RxCatchScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxTrampolineScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxCurrentThreadScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxEventLoopScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxImmediateScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxNewThreadScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxSceneTreeTimeoutScheduler>();
-	ClassDB::register_abstract_class<rx::wrappers::RxTimeoutScheduler>();
-	// observable
-	ClassDB::register_abstract_class<rx::wrappers::RxConnectableObservable>();
-	ClassDB::register_abstract_class<rx::wrappers::RxGroupedObservable>();
-	// observer
-	ClassDB::register_abstract_class<rx::wrappers::RxObserver>();
-	ClassDB::register_abstract_class<rx::wrappers::RxScheduledObserver>();
-	ClassDB::register_abstract_class<rx::wrappers::RxObserveOnObserver>();
-	ClassDB::register_abstract_class<rx::wrappers::RxAutoDetachObserver>();
-	// subject
-	ClassDB::register_abstract_class<rx::wrappers::RxSubject>();
-	ClassDB::register_abstract_class<rx::wrappers::RxReplaySubject>();
-	ClassDB::register_abstract_class<rx::wrappers::RxBehaviorSubject>();
-	ClassDB::register_abstract_class<rx::wrappers::RxAsyncSubject>();
-	// notification
-	ClassDB::register_abstract_class<rx::wrappers::RxNotificationOnNext>();
-	ClassDB::register_abstract_class<rx::wrappers::RxNotificationOnError>();
-	ClassDB::register_abstract_class<rx::wrappers::RxNotificationOnCompleted>();
 	// GodotRx Singleton
 	ClassDB::register_abstract_class<rx::__GDRxSingleton__>();
 
